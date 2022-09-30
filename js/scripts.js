9e5f4d30d0e9b9f500cba12933843b2202c9c7f8
Md Minhaj Ul Islam <minhajislam674@gmail.com>
2022-09-30 14:13:55 +0200

    Added 4 pokemon objects with some properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-alert('Hello world');
+let pokemonList = [
+  { name: 'Bulbasaur',
+    type: ['grass', 'posion'],
+    height: 0.7,
+    abilities: ['Chlorophyll', 'Overgrow']
+  },
 
-let favoriteFood = 'Noodles';
+  { name: 'Metapod',
+    type: ['bug', 'posion'],
+    height: 0.7,
+    abilities: 'Shed-skin'
+  },
 
-document.write(favoriteFood);
+  { name: 'Butterfree',
+    type: ['bug', 'flying'],
+    height: 1.1,
+    abilities: ['Compoundeyes', 'Tinted-lens']
+  },
+
+  { name: 'Vulpix',
+    type: 'fire',
+    height: 0.6,
+    abilities: ['Chlorophyll', 'Overgrow']
+  }
+];
