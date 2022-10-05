edfd17af562b531eaebfd9c0aba7b98cb4de1dc9
Md Minhaj Ul Islam <minhajislam674@gmail.com>
2022-10-05 16:18:47 +0200

    Wrapped pokemonList array in an IIFE
    Additionally forEach() function was used instead of for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,75 @@
-let pokemonList = [
-  { name: 'Bulbasaur',
-    types: ['grass', 'posion'],
-    height: 0.7,
-    abilities: ['Chlorophyll', 'Overgrow']
-  },
+//The Pokémon list is defined and housed within an IIFE.
 
-  { name: 'Metapod',
-    types: ['bug', 'posion'],
-    height: 0.7,
-    abilities: 'Shed-skin'
-  },
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'Bulbasaur',
+      types: ['grass', 'posion'],
+      height: 0.7,
+      abilities: ['Chlorophyll', 'Overgrow']
+    },
 
-  { name: 'Butterfree',
-    types: ['bug', 'flying'],
-    height: 1.1,
-    abilities: ['Compoundeyes', 'Tinted-lens']
-  },
+    { name: 'Metapod',
+      types: ['bug', 'posion'],
+      height: 0.7,
+      abilities: 'Shed-skin'
+    },
 
-  { name: 'Vulpix',
+    { name: 'Butterfree',
+      types: ['bug', 'flying'],
+      height: 1.1,
+      abilities: ['Compoundeyes', 'Tinted-lens']
+    },
+
+    { name: 'Vulpix',
+      types: 'fire',
+      height: 0.6,
+      abilities: ['Chlorophyll', 'Overgrow']
+    }
+  ];
+
+//The following two functions are what will allow anything outside the IIFE to interact with the pokemonList variable within it.
+
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+//The IIFE then returns an object with two keys: add and getAll.
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+
+console.log(pokemonRepository.getAll()); // returns pokemonList Array which has 4 pokemons
+
+
+// Accesing t
+pokemonRepository.add(
+  { name: 'Charmander',
     types: 'fire',
-    height: 0.6,
-    abilities: ['Chlorophyll', 'Overgrow']
-  }
-];
+    height: 2,
+    abilities: 'Blaze'
+});
+
+console.log(pokemonRepository.getAll()); // returns pokemonList Array which has now 5 pokemons
 
 
-// Using for loop to go through the name and height of the array
-
-/* for (let i = 0; i < pokemonList.length; i++) {
-
-// Writes the Pokémon name on your website’s DOM (Also HTML tags h2 and div were used to style)
-  document.write("<div>"+
-  "<h2>"+ pokemonList[i].name + " (height: " + pokemonList[i].height + " m)"+ "<h2>"+
-  "</div>");
-}
-*/
-
-// Using both loop and conditional to highlight special pokemon
-for (let i = 0; i < pokemonList.length; i++) {
-
-  // Setting the condition for big pokemon: any pokemon with a height for than 1.0 m is considered big
-  // HTML tag h2 is used to make them each pokemon appear in seperate lines
-  if (pokemonList[i].height > 1.0) {
-    document.write("<h2>"+ pokemonList[i].name + " (height: " + pokemonList[i].height + " m)"+ " - Wow, that’s big!" + "<h2>");
-
-  // Setting the condition for medium pokemon: any pokemon with a height for between 0.7 and 1.0 m is considered medium
-  } else if (pokemonList[i].height >= 0.7 && pokemonList[i].height <= 1.0) {
-    document.write("<h2>"+ pokemonList[i].name + " (height: " + pokemonList[i].height + " m)"+ " - a medium-sized pokemon" + "<h2>");
-
-  // All others are considered small
+//Using forEach() loops
+function myPokemonFunction(pokemon){
+  if (pokemon.height > 1.0) {
+    document.write("<h2>"+ pokemon.name + " (height: " + pokemon.height + " m)"+ " - Wow, that’s big!" + "<h2>");
+  } else if (pokemon.height >= 0.7 && pokemon.height <= 1.0) {
+    document.write("<h2>"+ pokemon.name + " (height: " + pokemon.height + " m)"+ " - a medium-sized pokemon" + "<h2>");
   } else {
-    document.write("<h2>"+ pokemonList[i].name + " (height: " + pokemonList[i].height + " m)"+ " - it's a small pokemon" + "<h2>");
+    document.write("<h2>"+ pokemon.name + " (height: " + pokemon.height + " m)"+ " - it's a small pokemon" + "<h2>");
   }
 }
+
+pokemonRepository.getAll().forEach(myPokemonFunction);
